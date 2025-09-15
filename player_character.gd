14997faef60be2b0d63fd690a6ffec2a086435f8
AdamaNII <adam@arpe.eu>
2025-09-15 14:16:52 +0200

    Add tilemap

--- player_character.gd
+++ player_character.gd
@@ -4,10 +4,16 @@ extends CharacterBody2D
 @export var camera: Camera2D
 @export var spritesheet: Texture
 
+var sprite: Sprite2D
+
 var speed = 48
+var facing = "down"
+
+var targetTile: Vector2
 
 func _enter_tree() -> void:
-	$Sprite2D.texture = spritesheet
+	sprite = $Sprite2D
+	sprite.texture = spritesheet
 
 func _physics_process(delta: float) -> void:
 	
@@ -16,8 +22,34 @@ func _physics_process(delta: float) -> void:
 	elif character == "water":
 		velocity = Input.get_vector("water_move_left", "water_move_right", "water_move_up", "water_move_down") * speed
 	
+	if velocity.y < 0:
+		sprite.region_rect.position.y = 64
+		facing = "up"
+	elif velocity.y > 0:
+		sprite.region_rect.position.y = 0
+		facing = "down"
+	if velocity.x < 0:
+		sprite.region_rect.position.y = 32
+		facing = "left"
+	elif velocity.x > 0:
+		sprite.region_rect.position.y = 96
+		facing = "right"
+	
 	move_and_slide()
 
 func _process(delta: float) -> void:
 	camera.position = position
 	z_index = position.y
+	
+	targetTile = Vector2(round((position.x - 8)/16), round((position.y)/16))
+	match facing:
+		"up":
+			targetTile.y -= 1
+		"down":
+			targetTile.y += 1
+		"left":
+			targetTile.x -= 1
+		"right":
+			targetTile.x += 1
+	
+	$TargetMarker.global_position = targetTile * 16 + Vector2(8, 8)
--- splitscreen.gd
+++ splitscreen.gd
@@ -4,7 +4,7 @@ extends Node2D
 @onready var viewport2 = $CanvasLayer/SubViewportContainer2/SubViewport
 @onready var camera1 = $CanvasLayer/SubViewportContainer/SubViewport/Camera2D
 @onready var camera2 = $CanvasLayer/SubViewportContainer2/SubViewport/Camera2D
-@onready var world = $CanvasLayer/SubViewportContainer/SubViewport/World
+@onready var world = $World
 
 func _ready():
 	viewport2.world_2d = viewport1.world_2d
