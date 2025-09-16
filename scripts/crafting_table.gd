534047944aaa14a53a75e305db1010af5b08699a
AdamaNII <adam@arpe.eu>
2025-09-16 15:00:08 +0200

    Starting Crafting System

---
+++ scripts/crafting_table.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var occupied = false
+var occupant = null
+
+var slots = {}
+
+var recipes_path = "res://resources/recipes"
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	pass
